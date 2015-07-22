d884c876b079759a2b040d26392816452ed4eea1
dhernz <d.hernz@gmail.com>
2015-07-22 15:13:04 -0700

    Completed first spec test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+	var statusCode;
+
+	var indexData = null;
+	if (req.method === 'GET') {
+		fs.readFile(archive.paths.index, function (err, data) {
+			if (err) throw err;
+			indexData = data.toString();
+		});
+		statusCode = 200;
+	}
+
+	res.end("/<input/");
 };
