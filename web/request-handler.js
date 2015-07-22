0ee5cf1d2c15584394950f6d7c69be843914e50f
dhernz <d.hernz@gmail.com>
2015-07-22 15:52:20 -0700

    Completed 2nd test

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,16 +5,37 @@ var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-	var statusCode;
-
+	
 	var indexData = null;
-	if (req.method === 'GET') {
-		fs.readFile(archive.paths.index, function (err, data) {
-			if (err) throw err;
-			indexData = data.toString();
-		});
-		statusCode = 200;
-	}
+  var website = null;
 
-	res.end("/<input/");
+	if (req.method === 'GET') {
+
+    // Find out url of req object
+    console.log('req.url', req.url);
+    if(req.url === '/'){
+      fs.readFile(archive.paths.index, function (err, data) {
+       if (err) throw err;
+       indexData = data.toString();
+      // console.log('indexData', indexData);  
+      res.end(indexData);
+    });
+
+    } else {
+      website = req.url;
+
+      fs.readFile(archive.paths.archivedSites + website, function (err, data) {
+       if (err) throw err;
+       indexData = data.toString();
+      // console.log('indexData', indexData);  
+      res.end(indexData);
+    });
+    }
+ 
+  }
+
 };
+
+
+//@params url
+// 
